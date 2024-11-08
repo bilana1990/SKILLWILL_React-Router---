8fa84f45b838c4681e68049f1921d84d641d79c3
gio bilana <giobilana@gmail.com>
2024-11-08 06:41:51 +0400

    Add new files

---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+// src/pages/Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>You can contact us here.</p>
+    </div>
+  );
+}
+
+export default Contact;
